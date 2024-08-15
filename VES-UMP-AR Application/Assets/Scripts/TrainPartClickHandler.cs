aba3f722c518270b02249079c718f19227cce18f
HaBiBo-art <ferianihabib4@gmail.com>
2024-08-15 09:30:30 +0100

    added a select highlighter and button to show interior scenes

--- VES-UMP-AR Application/Assets/Scripts/TrainPartClickHandler.cs
+++ VES-UMP-AR Application/Assets/Scripts/TrainPartClickHandler.cs
@@ -1,10 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TrainPartClickHandler : MonoBehaviour
 {
+    public Material selectedMaterial;  // Assign this in the Unity Inspector
+    private Material[] originalMaterials;
+    private GameObject selectedObject;
+    public Button showInteriorButton;
+
+    private string selectedScene;
+
+    
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -24,14 +32,81 @@ public class TrainPartClickHandler : MonoBehaviour
 
     void HandleClick(GameObject clickedObject)
     {
-        if (clickedObject.name == "PilotCabin")
+        // Check if the clicked object is one of the specific parts
+        if (clickedObject.name == "PilotCabin" || clickedObject.name == "PassengerCabin")
         {
-            SceneManager.LoadScene("PilotCabinInteriorScene");
+            // Reset the previous object's material
+            if (selectedObject != null)
+            {
+                ResetMaterials(selectedObject);
+                showInteriorButton.gameObject.SetActive(false);
+            }
+
+            // Highlight the new selected object
+            selectedObject = clickedObject;
+            SetMaterials(selectedObject, selectedMaterial);
+
+            // Determine which scene to load based on the clicked object
+            if (clickedObject.name == "PilotCabin")
+            {
+                selectedScene = "PilotCabinInteriorScene";
+            }
+            else if (clickedObject.name == "PassengerCabin")
+            {
+                selectedScene = "PassengerCabinInteriorScene";
+            }
+
+            // Show the button only if a valid part is selected
+            if (!string.IsNullOrEmpty(selectedScene))
+            {
+                showInteriorButton.gameObject.SetActive(true);
+            }
         }
-        else if (clickedObject.name == "PassengerCabin")
+        else
         {
-            SceneManager.LoadScene("PassengerCabinInteriorScene");
+            // If the clicked object is not one of the specific parts, reset selection
+            if (selectedObject != null)
+            {
+                ResetMaterials(selectedObject);
+                selectedObject = null;
+                showInteriorButton.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void SetMaterials(GameObject obj, Material material)
+    {
+        Renderer objRenderer = obj.GetComponent<Renderer>();
+        if (objRenderer != null)
+        {
+            // Save the original materials
+            originalMaterials = objRenderer.materials;
+            // Create a new array of the same length as the original materials
+            Material[] newMaterials = new Material[originalMaterials.Length];
+            // Replace all materials with the selected material
+            for (int i = 0; i < newMaterials.Length; i++)
+            {
+                newMaterials[i] = material;
+            }
+            objRenderer.materials = newMaterials;
+        }
+    }
+
+    void ResetMaterials(GameObject obj)
+    {
+        Renderer objRenderer = obj.GetComponent<Renderer>();
+        if (objRenderer != null && originalMaterials != null)
+        {
+            // Reset the materials to the original ones
+            objRenderer.materials = originalMaterials;
+        }
+    }
+
+    public void OnShowInteriorButtonClick()
+    {
+        if (!string.IsNullOrEmpty(selectedScene))
+        {
+            SceneManager.LoadScene(selectedScene);
         }
     }
 }
-
