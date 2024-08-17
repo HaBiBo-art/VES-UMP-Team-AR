261ae3e8d81e5c23fd9ed34ba366e58e3905ff53
HaBiBo-art <ferianihabib4@gmail.com>
2024-08-17 17:00:04 +0100

    added scene loader

---
+++ VES-UMP-AR Application/Assets/Scripts/SceneNavigation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigation : MonoBehaviour
+{
+    public void GoBackToMainScene()
+    {
+        // Reload the main scene named "MainScene", reinitializing all AR components
+        SceneManager.LoadScene("MainScene");
+    }
+}
+
+
--- VES-UMP-AR Application/Assets/Scripts/TrainPartClickHandler.cs
+++ VES-UMP-AR Application/Assets/Scripts/TrainPartClickHandler.cs
@@ -109,4 +109,5 @@ public class TrainPartClickHandler : MonoBehaviour
             SceneManager.LoadScene(selectedScene);
         }
     }
+
 }
